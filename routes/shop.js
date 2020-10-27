e934abb1bfce3cf228629b1b8ee1781480835679
Volodymyr Moskalenko <vmoskalenko.work@gmail.com>
2020-10-27 20:39:47 +0200

    Hbs templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,4 @@
-    const path = require('path');
+const path = require('path');
 
 const express = require('express');
 
@@ -8,8 +8,16 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const products = adminData.products
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'})
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+     }) 
+  //we need to put products.length logic inside js code as hbs don't support any logic
 });
 
 module.exports = router;
